418e2c36e4b541d879870722945d6b2066b30d9f
zy <1661996964@qq.com>
2022-11-10 11:05:33 +0800

    注解配置声明事务

---
+++ src/main/java/com/zy/spring/jdbc/SpringApplication.java
@@ -0,0 +1,15 @@
+package com.zy.spring.jdbc;
+
+import com.zy.spring.jdbc.dao.EmployeeDao;
+import com.zy.spring.jdbc.entity.Employee;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        EmployeeDao employeeDao = context.getBean("employeeDao", EmployeeDao.class);
+        Employee employee = employeeDao.findById(3308);
+        System.out.println(employee);
+    }
+}
---
+++ src/main/java/com/zy/spring/jdbc/service/EmployeeService.java
@@ -0,0 +1,51 @@
+package com.zy.spring.jdbc.service;
+
+import com.zy.spring.jdbc.dao.EmployeeDao;
+import com.zy.spring.jdbc.entity.Employee;
+
+import java.util.Date;
+
+public class EmployeeService {
+    private EmployeeDao employeeDao;
+    private BatchService batchService;
+
+    public void batchImport() {
+        for (int i = 1; i <= 10; i++) {
+            /*if(i==3){
+                throw new RuntimeException("意料之外的异常");
+            }*/
+            Employee employee = new Employee();
+            employee.setEno(8000 + i);
+            employee.setEname("员工" + i);
+            employee.setSalary(4000f);
+            employee.setDname("市场部");
+            employee.setHiredate(new Date());
+            employeeDao.insert(employee);
+        }
+    }
+
+    public void startImportJob(){
+        batchService.importJob1();
+        if(1==1){
+            throw new RuntimeException("意料之外的异常");
+        }
+        batchService.importJob2();
+        System.out.println("批量导入成功");
+    }
+
+    public EmployeeDao getEmployeeDao() {
+        return employeeDao;
+    }
+
+    public void setEmployeeDao(EmployeeDao employeeDao) {
+        this.employeeDao = employeeDao;
+    }
+
+    public BatchService getBatchService() {
+        return batchService;
+    }
+
+    public void setBatchService(BatchService batchService) {
+        this.batchService = batchService;
+    }
+}
